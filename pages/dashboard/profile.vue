193e9e7284edd2d0b62f19d4a4d9bb77b7c16425
Soumodip Paul <soumo@Soumodip-Paul.local>
2024-07-27 23:58:04 +0530

    Added new components

---
+++ pages/dashboard/profile.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-3xl font-bold">Profile</h1>
+        <account-information />
+
+        <edit-account-modal v-if="modal && modal.type === 'edit-account'" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+// Interface
+interface Modal {
+    type: string;
+    data: string;
+}
+
+const modal = ref(null as Modal | null);
+</script>
